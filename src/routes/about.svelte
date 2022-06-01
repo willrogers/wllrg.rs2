a8b4e38af7cad4c7c65732fcaf6499bbd2ef54b1
Will Rogers <wahrogers@gmail.com>
2022-06-01 21:09:40 +0100

    Skeleton of website.

---
+++ src/routes/about.svelte
@@ -0,0 +1,2 @@
+<h2>About me</h2>
+<p>I am a software engineer.</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Blog</h2>
+
+<p>
+	I write these posts more to focus my thoughts than in the expectation that others will read them,
+	but as you've found yourself here you can find me on
+	<a href="https://twitter.com/wrgrs">Twitter</a>.
+</p>
